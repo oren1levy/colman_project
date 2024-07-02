d31e60bf0265ca6c340ef9191f5a1fec67728714
yarinifrah12 <yarinifrah12@gmail.com>
2024-07-02 16:26:05 +0300

    yarin changes

---
+++ jaceScript/home.js
@@ -0,0 +1,7 @@
+document.getElementById('openMenu').addEventListener('click', function() {
+    document.getElementById('sideMenu').style.width = '250px';
+});
+
+document.getElementById('closeMenu').addEventListener('click', function() {
+    document.getElementById('sideMenu').style.width = '0';
+});
